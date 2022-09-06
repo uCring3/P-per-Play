0f8287d1f2e158e4c164005d795acbfae1472c75
Ping <108148486+uCring3@users.noreply.github.com>
2022-09-06 14:13:24 +0200

    Delete sound directory

--- sound/sfx.lua
+++
@@ -1,9 +0,0 @@
-sfx = {}
-
-function LOAD_SFX()
-
-	sfx.megalovenia_short = love.audio.newSource("sound/sfx/megalovenia_short.wav", "static")
-
-	love.audio.setEffect("Reverb", {type="reverb"})
-
-end
