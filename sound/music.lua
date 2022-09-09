3ea527dc8e208cba18e76c78c256c578ba55220a
Ping <108148486+uCring3@users.noreply.github.com>
2022-09-09 17:04:58 +0200

    Delete sound directory

--- sound/music.lua
+++
@@ -1,9 +0,0 @@
-music = {}
-
-function LOAD_MUSIC()
-	
-	music.kazotsky_kick = love.audio.newSource("sound/music/kazotsky_kick.mp3", "stream")
-
-	Bad_Millie = love.graphics.newVideo("sound/music/Bad_Millie.ogv")
-
-end
